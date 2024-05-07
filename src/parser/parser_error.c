6181f4804f05c7ef09952c6a4a933d1c6b7f2268
Enzo Signorotto <enzo.signorotto@protonmail.com>
2024-05-07 22:56:18 +0200

    Forgot some source files

---
+++ src/parser/parser_error.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include "parser_error.h"
+#include <stdio.h>
+
+// scope key must match with a dot
+
+bool parser_scan_errors(const Lexer* restrict lexer) {
+	const char* code = lexer->source->content;
+	size_t count_scope_nest = 0;
+
+	for(long int i = 0;
+	i < lexer->count - 1;
+	i += 1) {
+		const Token* token = &lexer->tokens[i];
+
+		if(token->type == TokenType_KEY) {
+			if(strcmp(
+				"scope",
+				&code[token->key_start])
+			<= 0)
+				count_scope_nest += 1;
+		} else if(token->subtype == TokenSubtype_PERIOD) {
+			if(count_scope_nest == 0)
+				return false;
+
+			count_scope_nest -= 1;
+		}
+	}
+
+	return count_scope_nest == 0;
+}
---
+++ src/parser/parser_error.h
@@ -0,0 +1,8 @@
+#ifndef PARSER_ERROR_H
+#define PARSER_ERROR_H
+
+#include "../lexer/lexer.h"
+
+bool parser_scan_errors(const Lexer* restrict lexer);
+
+#endif
